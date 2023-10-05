4ca5905e972d7e8948b53fe9b183978ce60c8312
Salsapila <salsabeelahmed323@gmail.com>
2023-10-05 09:56:11 -0700

    hellobettyworld

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for C
+ *
+ * Description: C program that prints with puts function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
