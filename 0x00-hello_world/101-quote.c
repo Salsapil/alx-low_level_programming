22e73e0ece415022d107f840c832dac0166e4c94
Salsapila <salsabeelahmed323@gmail.com>
2023-10-05 12:43:42 -0700

    msgadv

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program with standard error
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void))
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, msg, 59);
+	return (1);
+}
