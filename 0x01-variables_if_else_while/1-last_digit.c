f284c280217574555c74b461f79b758425ba6aa9
Salsapila <salsabeelahmed323@gmail.com>
2023-10-06 04:28:14 -0700

    ld ifelse

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: C program with if statments
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
+int n;
+int lastd;
 
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastd = n % 10;
+/* your code goes there */
+if (lastd > 5)
+{
+printf("%d is %s", n, "and is greater than 5");
+}
+else if (lastd == 0)
+{
+printf("%d is %s", n, "and is 0");
+}
+else
+printf("%d is %s", n, "and is less than 6 and not 0");
+return (0);
 }
