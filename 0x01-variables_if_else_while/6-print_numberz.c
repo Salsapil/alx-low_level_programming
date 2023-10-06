6828f0a229e4fc18cb420a082db554438d288b64
Salsapila <salsabeelahmed323@gmail.com>
2023-10-06 14:22:49 -0700

    displaydigits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point for c program
+ *
+ * Description: C program to print num.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
