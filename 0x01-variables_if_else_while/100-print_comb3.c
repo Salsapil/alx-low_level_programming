7a817971a3cb17465896093c9445d0aa863a64ac
Salsapila <salsabeelahmed323@gmail.com>
2023-10-07 03:26:54 -0700

    com digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: C program
+ *
+ * Return: Always 0 success
+*/
+int main(void)
+{
+int i;
+int l;
+for (i = 0; i <= 9; i++)
+{
+for (l = i + 1; l <= 9; l++)
+{
+putchar(i + '0');
+putchar(l + '0');
+if (i != 8 || l != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
