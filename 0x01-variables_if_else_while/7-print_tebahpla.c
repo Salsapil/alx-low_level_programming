ed7b559ebea54beae1ffbf531e314db1bb6ee555
Salsapila <salsabeelahmed323@gmail.com>
2023-10-07 00:28:22 -0700

    rev alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i = 'z';
+for (; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
