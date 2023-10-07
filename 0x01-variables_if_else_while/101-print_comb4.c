912a8c07fda67ce11f623a304e18bf40b1dbf233
Salsapila <salsabeelahmed323@gmail.com>
2023-10-07 04:10:50 -0700

    three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: C program
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+int l;
+int j;
+for (j = 0; j <= 7; j++)
+{
+for (i = 0; i <= 9; i++)
+{
+for (l = i + 1; l <= 9; l++)
+{
+putchar(j + '0');
+putchar(i + '0');
+putchar(l + '0');
+if (i != 8 || l != 9 || j != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
