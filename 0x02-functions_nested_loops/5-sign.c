f45e2255079d7e177349e305504caa2fc58d866e
Salsapila <salsabeelahmed323@gmail.com>
2023-10-10 14:13:37 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,27 @@
 #include "main.h"
-/***/
-
+/**
+ * print_sign - function
+ * @n: check input
+ * Return: 0 or 1
+*/
+int print_sign(int n)
+{
+int plus = 43;
+int zero = 48;
+int dash = 45;
+if (n > 0)
+{
+return (1);
+_putchar(plus);
+}
+else if (n < 0)
+{
+return (-1);
+_putchar(dash);
+}
+else
+{
+return (0);
+_putchar(zero);
+}
+}
