1e0a9d53d7fb8727563f1895234691e8cd88a9a5
Salsapila <salsabeelahmed323@gmail.com>
2023-10-10 17:16:57 -0700

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - function
  * Return: value
@@ -6,25 +7,16 @@
 */
 void print_to_98(int n)
 {
-int x, y;
-if (n >= 98)
+int i;
+if (n <= 98)
 {
-for (x = n; x > 98; x--)
-{
-_putchar(x + '0');
-}
-}
-else if (n <= 98)
-{
-for (y = n; y < 98; y++)
-{
-_putchar(y + '0');
-}
+for (i = n; i < 98; i++)
+printf("%d, ", i);
 }
 else
 {
-_putchar(n + '0');
+for (i = n; i > 98; i--)
+printf("%d, ", i);
 }
-_putchar(',');
-_putchar(' ');
+printf("98\n");
 }
