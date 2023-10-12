9571464423e4e587df6c3daa37b34a168c06372f
Salsapil <salsabeelahmed484@outlook.com>
2023-10-12 10:07:21 +0300

    uppercases

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - function
+ *
+ * @c: check input
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+    char c;
+    if (c = 'A')
+    {
+        return (1);
+    }
+    else if (c = 'a')
+    {
+        return (0);
+    }
+    return (0);
+}
