f8df83a624067c38e5f62e6325eab0a1a1dc6ebe
Salsapil <salsabeelahmed484@outlook.com>
2023-10-12 12:54:25 +0300

    tentimes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - function
+ * Return: 0
+ */
+void more_numbers(void)
+{
+    int i, k;
+
+    for (k = 0; k < 10; k++)
+    {
+        for (i = 0; i <= 14; i++)
+        {
+            if ( i >= 10)
+            {
+                _putchar(i / 10 + '0');
+                _putchar(i % 10 + '0');
+            }
+            else
+            {
+            _putchar(i + '0');
+            }
+        }
+            _putchar('\n');
+    }
+}
