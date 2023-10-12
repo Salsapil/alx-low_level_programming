e4e19a3e745f0cdcde982b64d939fea0d11da8d0
Salsapil <salsabeelahmed484@outlook.com>
2023-10-12 11:43:41 +0300

    mulnospaces

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,12 @@
 #include "main.h"
-/***/
+/**
+ * mul - function
+ *
+ * @a: check input
+ * @b: check input 
+ * Return: always 0
+*/
 int mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
