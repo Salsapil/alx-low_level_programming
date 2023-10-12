f2e1bc6706a450d5c0d4c4a1ad7ffb36d568efb4
Salsapil <salsabeelahmed484@outlook.com>
2023-10-12 10:53:57 +0300

    digitnospaces

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,15 +4,15 @@
  *
  * @c: check input
  * Return: always 0
- */
+*/
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-    {
-        return (1);
-    }
-    else 
-    {
-        return (0);
-    }
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else 
+{
+return (0);
+}
 }
