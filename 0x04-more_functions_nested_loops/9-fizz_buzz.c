64f36b61aa215a5dd9732b9edb6fa6eaff73d576
Salsapil <salsabeelahmed484@outlook.com>
2023-10-12 16:18:52 +0300

    fizzbuxx

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,33 +4,58 @@
  *
  * Return: 0
  */
-int main(void)
+void fizz_buzz(void)
 {
 int i;
 
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
-{
-printf("%s", "FizzBuzz");
-}
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
 else if (i % 3 == 0)
-{
-printf("%s", "Fizz");
-}
+printf("Fizz ");
 else if (i % 5 == 0)
-{
-printf("%s", "Buzz");
-}
-else if (i == 100)
-{
-printf("%s", "Buzz");
-}
+printf("Buzz ");
 else
+printf("%d ", i);
+}
+printf("\n");
+}
+
+int main(void)
 {
-printf("%d", i);
-printf(" ");
-}
-}
+fizz_buzz();
 return (0);
 }
+
+// int main(void)
+// {
+// int i;
+
+// for (i = 1; i <= 100; i++)
+// {
+// if ((i % 3 == 0) && (i % 5 == 0))
+// {
+// printf("%s", "FizzBuzz");
+// }
+// else if (i % 3 == 0)
+// {
+// printf("%s", "Fizz");
+// }
+// else if (i % 5 == 0)
+// {
+// printf("%s", "Buzz");
+// }
+// else if (i == 100)
+// {
+// printf("%s", "Buzz");
+// }
+// else
+// {
+// printf("%d", i);
+// printf(" ");
+// }
+// }
+// return (0);
+// }
+
