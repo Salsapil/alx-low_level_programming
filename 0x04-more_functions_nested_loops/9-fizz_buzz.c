59580ee6eb1ab8d7ceccf12ae8d37363afd8d4f1
Salsapil <salsabeelahmed484@outlook.com>
2023-10-12 14:39:04 +0300

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("%s ", "FizzBuzz");
+}
+else if (i % 3 == 0)
 {
 printf("%s ", "Fizz");
 }
@@ -18,11 +22,7 @@ else if (i % 5 == 0)
 {
 printf("%s ", "Buzz");
 }
-else if ((i % 3 == 0) && (i % 5 == 0))
-{
-printf("%s ", "FizzBuzz");
-}
-else 
+else
 {
 printf("%d ", i);
 }
