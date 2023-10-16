b5e9b99278552cb69d50f27448ecc329c849e0ed
Salsapil <salsabeelahmed484@outlook.com>
2023-10-16 16:27:05 +0300

    revarr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - function
+ * @s: check input
+ * Return: void
+*/
+void rev_string(char *s)
+{
+int i;
+int len = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+len++;
+}
+for (i = len; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+}
