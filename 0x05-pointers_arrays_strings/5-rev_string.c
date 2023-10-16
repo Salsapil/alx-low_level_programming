f3e0b09070ee2023c6e03e8d3a83124aa4426970
Salsapil <salsabeelahmed484@outlook.com>
2023-10-17 00:19:58 +0300

    reverse string five

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,25 @@
 */
 void rev_string(char *s)
 {
-    int len;
-    int lenn;
-    int i;
-    char tem;
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    len--;
-    i = 0;
-    lenn = len;
-    while (lenn != len / 2)
-    {
-        tem = s[lenn];
-        s[lenn] = s[i];
-        s[i] = tem;
-        lenn--;
-        i++;
-    }
+int len;
+int lenn;
+int i;
+char tem;
+len = 0;
+lenn = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+len--;
+i = 0;
+lenn = len;
+while (lenn > 0)
+{
+tem = s[lenn];
+s[lenn] = s[i];
+s[i] = tem;
+lenn--;
+i++;
+}
 }
