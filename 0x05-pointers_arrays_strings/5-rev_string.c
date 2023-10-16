4e37b95560d2384022050a26785a8c4515a64d2b
Salsapil <salsabeelahmed484@outlook.com>
2023-10-17 00:04:56 +0300

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,4 +6,23 @@
 */
 void rev_string(char *s)
 {
+    int len;
+    int lenn;
+    int i;
+    char tem;
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    len--;
+    i = 0;
+    lenn = len;
+    while (lenn != len / 2)
+    {
+        tem = s[lenn];
+        s[lenn] = s[i];
+        s[i] = tem;
+        lenn--;
+        i++;
+    }
 }
