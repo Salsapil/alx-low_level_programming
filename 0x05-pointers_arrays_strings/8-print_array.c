f4f321a9e2ff110090a563405f488fd47c293b04
Salsapil <salsabeelahmed484@outlook.com>
2023-10-16 19:17:44 +0300

    arraypointer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - function
+ * @a: check input 
+ * @n: check input
+ * Return: void
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+if (i != n-1)
+{
+printf(", ");
+}
+i++;
+}
+}
