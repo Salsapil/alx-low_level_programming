c52570c9adfead13adc4498ba90dd0e9736feb92
Salsapil <salsabeelahmed484@outlook.com>
2023-10-19 01:49:42 +0300

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - function
+ * @a: check input
+ * @n: check input
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int tem;
+for (i = 0; i < n / 2; i++)
+{
+tem = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = tem;
+}
+a[i] = '\0';
+}
