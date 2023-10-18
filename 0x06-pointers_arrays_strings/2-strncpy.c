b6d76551f5466e82666085c51bb4969423df0fb7
Salsapil <salsabeelahmed484@outlook.com>
2023-10-19 00:33:45 +0300

    copying

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,21 @@
 #include "main.h"
-/***/
+/**
+ * _strncpy - function
+ * @dest: check input
+ * @src: check input
+ * @n: check input
+ * Return: value
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-    
+int i;
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
