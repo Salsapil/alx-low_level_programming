156c62f21ddce3372bdceb17ddfffcb0c393b5a9
Salsapil <salsabeelahmed484@outlook.com>
2023-10-23 22:45:34 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,25 @@
 #include "main.h"
-/***/
+/**
+ * _strpbrk - function
+ * @s: 
+ * @accept:
+ * Return: value
+*/
 char *_strpbrk(char *s, char *accept)
 {
-    
+int i;
+int j;
+char *p;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+{
+p = &s[i];
+return (p);
+}
+}
+}
+return (0);
 }
