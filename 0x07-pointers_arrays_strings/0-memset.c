e80441e07a2a35048dfec7a4329a31b520fc3db0
Salsapil <salsabeelahmed484@outlook.com>
2023-10-23 19:18:49 +0300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
  * _memset - function
- *
  * @s: check input
  * @b: check input
  * @n: check input
- * Return: value 
+ * Return: value
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i;
-    for (i = 0; n > 0; i++)
-    {
-        s[i] = b;
-        n--;
-    }
-    return (0);
+int i;
+for (i = 0; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
 }
