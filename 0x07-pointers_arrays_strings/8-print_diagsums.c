56d3d6f0f2757824e1858e7baecf4c207751ee49
Salsapil <salsabeelahmed484@outlook.com>
2023-10-23 23:30:39 +0300

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,22 @@
 #include "main.h"
-/***/
+#include <stdio.h>
+/**
+ * print_diagsums - function
+ * @a: check input
+ * @size: check input
+ * Return: void
+*/
 void print_diagsums(int *a, int size)
 {
-    
+int i, sum, sum1;
+sum = 0;
+sum1 = 0;
+for (i = 0; i < size; i++)
+{
+sum += a[i];
+sum1 += a[size - i - 1];
+a += size;
+}
+printf("%d, ", sum);
+printf("%d\n", sum1);
 }
