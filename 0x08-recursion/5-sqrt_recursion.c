a1898139e109549fe9c8d1de8b3a706e976978a1
Salsapil <salsabeelahmed484@outlook.com>
2023-10-26 00:12:02 +0300

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,35 @@
 #include "main.h"
-/***/
+/**
+ * _sqrt_recursion_two - function
+ * @i: check input
+ * @num: check input
+ * Return: value
+*/
+int _sqrt_recursion_two(int i, int num)
+{
+if (i * i == num)
+{
+return (i);
+}
+else 
+{
+if (i * i > num)
+{
+return (-1);
+}
+return (_sqrt_recursion_two(i + 1, num));
+}
+}
+/**
+ * _sqrt_recursion - function
+ * @n: check input
+ * Return: value
+*/
 int _sqrt_recursion(int n)
 {
-    if (n == 0 || n == 1)
-    {
-        return (n);
-    }
-    if ( _sqrt_recursion(n - 1) < (n / _sqrt_recursion(n - 1)))
-    {
-        return ( _sqrt_recursion(n - 1));
-    }
-    else
-    {
-        return (n / _sqrt_recursion(n - 1));
-    }
+if (n < 0)
+{
+return (-1);
+}
+return (_sqrt_recursion_two(0, n));
 }
