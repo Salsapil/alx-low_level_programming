2949d928c6da6a1c85b49bf2f9365a27c322bf3c
Salsapil <salsabeelahmed484@outlook.com>
2023-10-25 23:56:59 +0300

    primenumbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,5 +2,16 @@
 /***/
 int _sqrt_recursion(int n)
 {
-    
+    if (n == 0 || n == 1)
+    {
+        return (n);
+    }
+    if ( _sqrt_recursion(n - 1) < (n / _sqrt_recursion(n - 1)))
+    {
+        return ( _sqrt_recursion(n - 1));
+    }
+    else
+    {
+        return (n / _sqrt_recursion(n - 1));
+    }
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,32 @@
 #include "main.h"
-/***/
+/**
+ * is_prime_two - function
+ * @n: check input
+ * @i: check input
+ * Return: values
+*/
+int is_prime_two(int n, int i)
+{
+if (i == n)
+{
+return (1);
+}
+if (n % i == 0)
+{
+return (0);
+}
+return (is_prime_two(n, i + 1));
+}
+/**
+ * is_prime_number - function
+ * @n: check input
+ * Return: value
+*/
 int is_prime_number(int n)
 {
-    
+if (n <= 1)
+{
+return (0);
+}
+return (is_prime_two(n, 2));
 }
