dde6974ee6c101d1673dbdd1f40aa2710fad44de
Salsapil <salsabeelahmed484@outlook.com>
2023-10-27 21:04:07 +0200

    firsttask

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: check input
+ * @argv: check input
+ * Return: 0
+*/
+int main(int argc, char const *argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
