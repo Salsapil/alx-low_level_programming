95dd409c9f797e61f33ab51d01ab4f2ea8e7fa0e
Salsapil <salsabeelahmed484@outlook.com>
2023-10-27 21:13:43 +0200

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: check input
+ * @argv: check input
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
