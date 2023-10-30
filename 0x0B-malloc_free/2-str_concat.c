eda9da47cbdd323b9fabd7130e16f9f472e7d855
Salsapil <salsabeelahmed484@outlook.com>
2023-10-30 23:00:20 +0200

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - function
+ * @s1: check input
+ * @s2: check input
+ * Return: value
+*/
+char *str_concat(char *s1, char *s2)
+{
+    char *m;
+    int i, j, k;
+    int size1, size2;
+    int len;
+    len = 0;
+    size1 = 0;
+    size2 = 0;
+    for (i = 0; i < s1[i]; i++)
+    {
+        size1++;
+    }
+    for (j = 0; j < s2[j]; j++)
+    {
+        size2++;
+    }
+    len = size1 + size2;
+    m = malloc((len) * sizeof(char) + 1);
+    if (m == 0)
+    {
+        return (0);
+    }
+    for (k = 0; k < len; k++)
+    {
+        if (k < size1)
+        {
+            m[k] = s1[k];
+        }
+        else
+            m[k] = s2[k - size2];
+    }
+    m[k] = '\0';
+    return (m);
+}
