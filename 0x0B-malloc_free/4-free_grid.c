8d7db16c24c020386728d1a12d069ad5d1c55c74
Salsapil <salsabeelahmed484@outlook.com>
2023-10-31 11:57:27 +0200

    freegrid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/***/
+/**
+ * free_grid - function
+ * @grid: check input
+ * @height: check input
+ * Return: void
+*/
 void free_grid(int **grid, int height)
 {
-    
+int i;
+i = 0;
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid);
 }
