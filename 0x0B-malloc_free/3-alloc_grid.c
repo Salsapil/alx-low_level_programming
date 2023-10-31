483879ccfda2dd36502ed4b80e502487b9c28155
Salsapil <salsabeelahmed484@outlook.com>
2023-10-31 11:19:22 +0200

    2D

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,5 +4,29 @@
 /***/
 int **alloc_grid(int width, int height)
 {
-    
+int **m, i, j;
+m = malloc(sizeof(m) * height);
+if (width <= 0 || height <= 0 || m == 0)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < height; i++)
+{
+m[i] = malloc(sizeof(**m) * width);
+if (m[i] == 0)
+{
+while (i--)
+{
+free(m[i]);
+}
+free(m);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+m[i][j] = 0;
+}
+}
+return (m);
 }
