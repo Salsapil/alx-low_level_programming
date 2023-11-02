5ccb12c6e7bd95b40a4e69f471b9768733f9223a
Salsapil <salsabeelahmed484@outlook.com>
2023-11-02 17:24:24 +0200

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,16 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * malloc_checked - function
  * @b: check input
  * Return: void
-*/
+ */
 void *malloc_checked(unsigned int b)
 {
-int *m;
-m = malloc(b);
+int *m = malloc(b);
 if (m == 0)
 {
 exit(98);
 }
-else
-{
 return (m);
-}
 }
