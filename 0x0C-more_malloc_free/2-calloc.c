496a81dde84793f2421b8f070d0e874749e01aa6
Salsapil <salsabeelahmed484@outlook.com>
2023-11-02 20:59:22 +0200

    thirdtask

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
-/***/
+/**
+ * _callo - function
+ * @nmemb: check input
+ * @size: check input
+ * Return: void
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    
+char *ptr;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < nmemb * size; i++)
+*(ptr + i) = 0;
+return ((void *)ptr);
 }
