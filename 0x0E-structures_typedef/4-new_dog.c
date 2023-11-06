0513c9c6ee9f5f5fcf445371bcb63fa91007191f
Salsapil <salsabeelahmed484@outlook.com>
2023-11-07 01:11:35 +0200

    taskfour

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,16 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
 /***/
 int lenght(const char *str)
 {
-    int len, i;
+    int len;
+    int i;
     len = 0;
     for (i = 0; str[i] != '\0'; i++)
     {
         len++;
     }
-    return (len)
+    return (len);
 }
 char *copy(char *dest, char *str)
 {
@@ -18,10 +19,36 @@ char *copy(char *dest, char *str)
     {
         dest[i] = str[i];
     }
-    dest[i] = "\0";
+    dest[i] = '\0';
     return (dest);
 }
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    
+    dog_t *m;
+    if (age < 0 || !name || !owner)
+    {
+        return (NULL);
+    }
+    m = malloc(sizeof(dog_t));
+    if (m == NULL)
+    {
+        return (NULL);
+    }
+    m->name = malloc(sizeof(char) * lenght(name) + 1);
+    if (m->name == NULL)
+    {
+        free(m);
+        return (NULL);
+    }
+    m->owner = malloc(sizeof(char) * lenght(name) + 1);
+    if (m->owner == NULL)
+    {
+        free(m->name);
+        free(m);
+        return (NULL);
+    }
+    m->name = copy(m->name, name);
+    m->age = age;
+    m->owner = copy(m->owner, owner);
+    return (m);
 }
