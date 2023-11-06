f7d19252ca2c3f275762e7a4f00e32be41b5d5cd
Salsapil <salsabeelahmed484@outlook.com>
2023-11-06 23:00:22 +0200

    tasktwo

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,17 @@
-#ifndef DOG_H
-#define DOG_H
+#include "dog.h"
 /**
- * struct dog - function
+ * init_dog - function
+ * @d: struct
  * @name: name
  * @age: age
  * @owner: owner
 */
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
 void init_dog(struct dog *d, char *name, float age, char *owner)
-#endif
+{
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
