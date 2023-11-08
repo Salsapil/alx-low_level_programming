64f949e131d17a91fbc3b181d34bc29c606444d0
Salsabil <salsabeelahmed323@gmail.com>
2023-11-08 14:43:58 +0200

    secondtask

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - function
+ * @array: check input
+ * @size: check input
+ * @action: check input
+ * Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int *pointer;
+pointer = array + (size - 1);
+if (array && size && action)
+{
+while (array <= pointer)
+{
+action(*array++);
+}
+}
+}
