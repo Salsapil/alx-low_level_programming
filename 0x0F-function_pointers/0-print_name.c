d60f11cbb4e98895862b6e1caaef70ed097d6f88
Salsabil <salsabeelahmed323@gmail.com>
2023-11-08 14:05:54 +0200

    firsttask

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - function
  * @name: check input
@@ -7,5 +8,6 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL || f != NULL)
 f(name);
 }
