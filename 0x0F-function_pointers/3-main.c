8b7a4d5127873af2fc310e24b489fa323a9d7fc5
Salsabil <salsabeelahmed323@gmail.com>
2023-11-09 01:18:49 +0200

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,28 +9,28 @@
  */
 int main(int argc, char **argv)
 {
-    int (*f)(int, int);
-    int i, x, y;
-    char *s;
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    x = atoi(argv[1]);
-    y = atoi(argv[3]);
-    s = argv[2];
-    if (!x && !y)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    f = get_op_func(s);
-    if (!(f))
-    {
-        printf("Error\n");
-        exit(99);
-    }
-    i = f(x, y);
-    printf("%d\n", i);
+int (*f)(int, int);
+int i, x, y;
+char *s;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+x = atoi(argv[1]);
+y = atoi(argv[3]);
+s = argv[2];
+if (!x && !y)
+{
+printf("Error\n");
+exit(98);
+}
+f = get_op_func(s);
+if (!(f))
+{
+printf("Error\n");
+exit(99);
+}
+i = f(x, y);
+printf("%d\n", i);
 }
