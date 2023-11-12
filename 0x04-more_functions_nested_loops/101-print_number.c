963dbf45b364a58661b7bfc37e575ed763d608b2
Salsabil <salsabeelahmed323@gmail.com>
2023-11-13 00:30:15 +0200

    committask101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,22 @@
 #include "main.h"
 void print_number(int n)
 {
-    int i = 1;
-    unsigned int num;
+int i = 1;
+unsigned int num;
 
-    if (n < 0) {
-        _putchar('-');
-        num = -n;
-    } else {
-        num = n;
-    }
+if (n < 0) {
+_putchar('-');
+num = -n;
+} else {
+num = n;
+}
 
-    while (num / i > 9) {
-        i *= 10;
-    }
+while (num / i > 9) {
+i *= 10;
+}
 
-    while (i > 0) {
-        _putchar((num / i) % 10 + '0');
-        i /= 10;
-    }
+while (i > 0) {
+_putchar((num / i) % 10 + '0');
+i /= 10;
+}
 }
