e03e47b8e1a0029c237e37e5830f32e08cc813e6
Salsabil <salsabeelahmed323@gmail.com>
2023-11-16 23:39:41 +0200

    free memoey

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,19 @@
 #include "lists.h"
-/***/
+#include <stdlib.h>
+/**
+ * free_list - function
+ * @head: check input
+ * Return: void
+*/
 void free_list(list_t *head)
 {
-    
+list_t *tem;
+
+while (head != NULL)
+{
+tem = head;
+head = head->next;
+free(tem->str);
+free(tem);
+}
 }
