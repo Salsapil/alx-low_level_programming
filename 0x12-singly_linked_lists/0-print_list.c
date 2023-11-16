a66ecb919132373bab01fc931fcc9813818398f9
Salsabil <salsabeelahmed323@gmail.com>
2023-11-16 18:01:59 +0200

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/***/
+int _strlen(char *s)
+{
+    int i;
+    i = 0;
+    if (s == NULL)
+    {
+        return (0);
+    }
+    else
+    {
+        while (s[i] != '\0')
+        {
+            i++;
+        }
+        
+    }
+    return (i);
+}
+size_t print_list(const list_t *h)
+{
+size_t i;
+i = 0;
+while (h != NULL)
+{
+    if (!_strlen(h->str))
+    {
+        printf("[%d] %s\n", _strlen(h->str), "(nil)");
+    }
+    else
+    {
+        printf("[%d] %s\n", _strlen(h->str), h->str);
+
+    }
+    h = h->next;
+    i++;
+}
+return (i);
+}
