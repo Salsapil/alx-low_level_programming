c91a21da131621cfb90f30deec8be77c0db71e71
Salsabil <salsabeelahmed323@gmail.com>
2023-11-16 23:04:45 +0200

    adding node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,28 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-/***/
+/**
+ * add_node - function
+ * @head: check input
+ * @str: check input
+ * Return: value
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-    
+list_t *new;
+new = malloc(sizeof(list_t));
+if (!new)
+{
+return (NULL);
+}
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->next = *head;
+*head = new;
+return (new);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,5 +2,5 @@
 /***/
 list_t *add_node_end(list_t **head, const char *str)
 {
-    
+
 }
