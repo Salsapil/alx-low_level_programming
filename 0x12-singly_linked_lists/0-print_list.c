41e8ac8b36e2ed1beb9a0c607ff9c2aa115ecbdb
Salsabil <salsabeelahmed323@gmail.com>
2023-11-16 22:01:21 +0200

    edit first 2 tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,42 +1,51 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
-/***/
+/**
+ * _strlen - function
+ * @s: check input
+ * Return: value
+*/
 int _strlen(char *s)
 {
-    int i;
-    i = 0;
-    if (s == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        while (s[i] != '\0')
-        {
-            i++;
-        }
-        
-    }
-    return (i);
+int i;
+i = 0;
+if (s == NULL)
+{
+return (0);
 }
+else
+{
+while (s[i] != '\0')
+{
+i++;
+}
+
+}
+return (i);
+}
+/**
+ * print_list - function
+ * @h: check input
+ * Return: value
+*/
 size_t print_list(const list_t *h)
 {
 int i;
 i = 0;
 while (h != NULL)
 {
-    if (!_strlen(h->str))
-    {
-        printf("[%d] %s\n", _strlen(h->str), "(nil)");
-    }
-    else
-    {
-        printf("[%d] %s\n", _strlen(h->str), h->str);
+if (!h->str)
+{
+printf("[%d] %s\n", _strlen(h->str), "(nil)");
+}
+else
+{
+printf("[%d] %s\n", _strlen(h->str), h->str);
 
-    }
-    h = h->next;
-    i++;
+}
+h = h->next;
+i++;
 }
 return (i);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,17 @@
 #include "lists.h"
-/***/
+/**
+ * list_len - function
+ * @h: check input
+ * Return: value
+*/
 size_t list_len(const list_t *h)
 {
 int i;
 i = 0;
 while (h != NULL)
 {
-    h = h->next;
-    i++;
+h = h->next;
+i++;
 }
 return (i);
 }
