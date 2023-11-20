e4bb51949ce12ec2a15247828dd7fc9880618e31
Salsabil <salsabeelahmed323@gmail.com>
2023-11-21 00:37:10 +0200

    free case null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,15 @@
 void free_listint2(listint_t **head)
 {
 listint_t *tem;
-
+if (*head == NULL)
+{
+return;
+}
 while (*head != NULL)
 {
 tem = *head;
 *head = (*head)->next;
 free(tem);
 }
-head = NULL;
+*head = NULL;
 }
