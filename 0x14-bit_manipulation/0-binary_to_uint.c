33dff5a290a717c0800b339097d298f834cf6e52
Salsabil <salsabeelahmed323@gmail.com>
2023-11-23 18:36:17 +0200

    firsttask

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * binary_to_uint - function
+ * @b: input
+ * Return: value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int num;
+num = 0;
+
+if (b == NULL)
+return (0);
+while (*b)
+{
+if (*b != '0' && *b != '1')
+{
+return (0);
+}
+num <<= 1;
+num += (*b - '0');
+b++;
+}
+return (num);
+}
