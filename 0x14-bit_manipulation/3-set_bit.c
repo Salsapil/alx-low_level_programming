646cb0b764296e9f92c6d3f59de37728f0e0e564
Salsabil <salsabeelahmed323@gmail.com>
2023-11-24 00:48:00 +0200

    test task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,15 +7,12 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int shift_index;
-shift_index = 0;
 
 if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-shift_index = *n >> index;
-*n = shift_index | 1;
+*n = *n >> index | 1;
 *n = *n << index;
 return (1);
 }
