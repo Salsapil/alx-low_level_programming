a86cf2d647bb0d2afcdf91c685ad14fa928f856b
Salsabil <salsabeelahmed323@gmail.com>
2023-11-24 02:09:28 +0200

    set bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * set_bit - function
+ * clear_bit - function
  * @n: input
  * @index: input
  * Return: value
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-*n = *n | 1 << index;
+*n = *n & ~(1 << index);
 return (1);
 }
