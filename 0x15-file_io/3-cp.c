db4218b5c929cdadf3a67c38252c8286d81cc8d8
Salsabil <salsabeelahmed323@gmail.com>
2023-11-28 00:53:52 +0200

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,111 @@
 #include "main.h"
 #define BUF_SIZE 1024
+int open_file(const char *filename, int flags, mode_t mode);
+int copy_file(int fd_from, int fd_to, char *buf, size_t buf_size);
+int close_file(int fd);
+int print_error(const char *msg, const char *filename, int err_code);
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: value
+ * Return: 0 on success, or an error code on failure
  */
 int main(int argc, char **argv)
 {
-int cp_from, cp_to, n_read, n_written, close_status;
+int fd_from, fd_to, close_status;
 char buf[BUF_SIZE];
+
 if (argc != 3)
+return (print_error("Usage: cp file_from file_to", NULL, 97));
+fd_from = open_file(argv[1], O_RDONLY, 0);
+if (fd_from == -1)
+return (print_error("Error: Can't read from file", argv[1], 98));
+fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-return (97);
+close_file(fd_from);
+return (print_error("Error: Can't write to", argv[2], 99));
 }
-cp_from = open(argv[1], O_RDONLY);
-if (cp_from == -1)
+if (copy_file(fd_from, fd_to, buf, BUF_SIZE) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-return (98);
+close_file(fd_from);
+close_file(fd_to);
+return (print_error("Error: Can't copy file", NULL, 99));
 }
-cp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (cp_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(cp_from);
-return (99);
-}
-while ((n_read = read(cp_from, buf, BUF_SIZE)) > 0)
-{
-n_written = write(cp_to, buf, n_read);
-if (n_written == -1 || n_written != n_read)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(cp_from);
-close(cp_to);
-return (99);
-}
-}
-if (n_read == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(cp_from);
-close(cp_to);
-return (98);
-}
-close_status = close(cp_from);
+close_status = close_file(fd_from);
 if (close_status == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_from);
-close(cp_to);
-return (100);
-}
-close_status = close(cp_to);
+return (print_error("Error: Can't close fd", NULL, 100));
+close_status = close_file(fd_to);
 if (close_status == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_to);
-return (100);
-}
+return (print_error("Error: Can't close fd", NULL, 100));
 return (0);
+}
+
+/**
+ * open_file - opens a file with the specified name, flags, and mode
+ * @filename: name of the file to open
+ * @flags: flags to use for opening the file
+ * @mode: mode to use for opening the file
+ *
+ * Return: file descriptor on success, or -1 on failure
+ */
+int open_file(const char *filename, int flags, mode_t mode)
+{
+int fd = open(filename, flags, mode);
+return (fd);
+}
+
+/**
+ * copy_file - copies the contents of one file descriptor to another
+ * @fd_from: file descriptor to copy from
+ * @fd_to: file descriptor to copy to
+ * @buf: buffer to use for copying
+ * @buf_size: size of the buffer
+ *
+ * Return: 0 on success, or -1 on failure
+ */
+int copy_file(int fd_from, int fd_to, char *buf, size_t buf_size)
+{
+ssize_t n_read, n_written;
+
+while ((n_read = read(fd_from, buf, buf_size)) > 0)
+{
+n_written = write(fd_to, buf, n_read);
+if (n_written == -1 || n_written != n_read)
+return (-1);
+}
+
+if (n_read == -1)
+return (-1);
+
+return (0);
+}
+
+/**
+ * close_file - closes a file descriptor
+ * @fd: file descriptor to close
+ *
+ * Return: 0 on success, or -1 on failure
+ */
+int close_file(int fd)
+{
+int close_status = close(fd);
+return (close_status);
+}
+
+/**
+ * print_error - prints an error message to the standard error output
+ * @msg: message to print
+ * @filename: name of the file to include in the message
+ * @err_code: error code to return
+ *
+ * Return: err_code
+ */
+int print_error(const char *msg, const char *filename, int err_code)
+{
+if (filename == NULL)
+dprintf(STDERR_FILENO, "%s\n", msg);
+else
+dprintf(STDERR_FILENO, "%s %s\n", msg, filename);
+return (err_code);
 }
---
+++ 0x15-file_io/3-cp.s
@@ -0,0 +1,63 @@
+#include "main.h"
+#define BUF_SIZE 1024
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: value
+ */
+int main(int argc, char **argv)
+{
+int cp_from, cp_to, n_read, n_written, close_status;
+char buf[BUF_SIZE];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+return (97);
+}
+cp_from = open(argv[1], O_RDONLY);
+if (cp_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+return (98);
+}
+cp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (cp_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(cp_from);
+return (99);
+}
+while ((n_read = read(cp_from, buf, BUF_SIZE)) > 0)
+{
+n_written = write(cp_to, buf, n_read);
+if (n_written == -1 || n_written != n_read)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(cp_from);
+close(cp_to);
+return (99);
+}
+}
+if (n_read == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close(cp_from);
+close(cp_to);
+return (98);
+}
+close_status = close(cp_from);
+if (close_status == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_from);
+close(cp_to);
+return (100);
+}
+close_status = close(cp_to);
+if (close_status == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_to);
+return (100);
+}
+return (0);
+}
