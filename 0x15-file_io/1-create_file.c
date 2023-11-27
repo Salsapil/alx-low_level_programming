08f86d94817047f4e275be78fef183d21582631d
Salsabil <salsabeelahmed323@gmail.com>
2023-11-27 17:49:44 +0200

    testtask1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,23 +4,23 @@
 #include <unistd.h>
 #include <string.h>
 /**
- * read_textfile - function
+ * create_file - function
  * @filename: name of the file to create
  * @text_content: text content inside the file
- * Return:
+ * Return: value
  */
 int create_file(const char *filename, char *text_content)
 {
 int fd, n_written;
 if (filename == NULL)
-    return (-1);
+return (-1);
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
-    return (-1);
+return (-1);
 if (text_content != NULL)
 {
 n_written = write(fd, text_content, strlen(text_content));
-if (n_written)
+if (n_written == -1)
 {
 close(fd);
 return (-1);
