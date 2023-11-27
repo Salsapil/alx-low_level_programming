86bfa87634cc8b7e6b925edaf7da053cb152c9b3
Salsabil <salsabeelahmed323@gmail.com>
2023-11-28 00:10:07 +0200

    testing

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,4 +4,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int close_file(int fd);
+int copy_file(int cp_from, int cp_to, char *buf, size_t buf_size);
 #endif
