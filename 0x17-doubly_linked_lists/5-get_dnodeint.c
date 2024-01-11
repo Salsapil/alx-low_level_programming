a1ec8e6613976ea7a61b77dc3e68f46309b3aaf7
Salsabil <salsabeelahmed323@gmail.com>
2024-01-11 19:17:19 +0200

    task5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,5 +7,17 @@
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+    unsigned int i = 0;
+    dlistint_t *temp = head;
 
+    while (temp != NULL && i < index)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (i == index)
+        return (temp);
+
+    return (NULL);
 }
