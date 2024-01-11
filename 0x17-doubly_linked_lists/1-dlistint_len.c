7082330ad72069668019890839b3b3afabc3cacc
Salsabil <salsabeelahmed323@gmail.com>
2024-01-11 19:06:34 +0200

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
- * print_dlistint - function to print
+ * dlistint_len - function to print
  * @h: pointer to head
- * return: value
+ * Return: value
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i;
-    for (i = 0; h != NULL; i++)
-    {
-        h = h->next;
-    }
-    return (i);
+size_t i;
+for (i = 0; h != NULL; i++)
+{
+h = h->next;
+}
+return (i);
 }
