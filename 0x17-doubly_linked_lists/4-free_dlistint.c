d6f2cc73da06214d5ed767b3de3b83c923465baa
Salsabil <salsabeelahmed323@gmail.com>
2024-01-11 19:14:16 +0200

    task4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,9 +2,16 @@
 /**
  * free_dlistint - function to free
  * @head: pointer to head
- * return: void
-*/
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
+dlistint_t *temp;
 
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
