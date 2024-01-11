07ecf58054441eccc67670409840a395a7736aeb
Salsabil <salsabeelahmed323@gmail.com>
2024-01-11 19:24:59 +0200

    task6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -2,9 +2,18 @@
 /**
  * sum_dlistint - function
  * @head: pointer
- * return: value
+ * Return: value
 */
 int sum_dlistint(dlistint_t *head)
 {
-    
+int sum = 0;
+dlistint_t *temp = head;
+
+while (temp != NULL)
+{
+sum += temp->n;
+temp = temp->next;
+}
+
+return (sum);
 }
