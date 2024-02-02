978a57056d3be240374f3d8428681bef4db8b2b7
Salsabil <salsabeelahmed323@gmail.com>
2024-02-03 00:53:08 +0200

    Hash-Table100.1.2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -70,6 +70,7 @@ shash_node_t *stail;
 } shash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+shash_table_t *shash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
