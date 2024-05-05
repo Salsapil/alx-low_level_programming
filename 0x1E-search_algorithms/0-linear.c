f2465685872ab2669d3b6ffd34aa7e7bb96f1f80
Salsabil <salsabeelahmed323@gmail.com>
2024-05-05 15:37:11 +0300

    0x1E-search_algorithms- Task0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - Entry point
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: value or -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+size_t i = 0;
+if (array == NULL)
+{
+return (-1);
+}
+for (i; i < size; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+{
+return (i);
+}
+}
+return (-1);
+}
